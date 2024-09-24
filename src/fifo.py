5bdae5af2fac758b8fb5b0635a22c863a27ebb53
slottwo <41028091+slottwo@users.noreply.github.com>
2024-09-24 13:21:05 -0300

    done done fifo

--- src/fifo.py
+++ src/fifo.py
@@ -1,7 +1,3 @@
-# import queue
-
-
-from typing import Generator
 from process import Process
 
 
@@ -13,45 +9,21 @@ class Queue:
         else:
             raise ValueError
 
-    def __getitem__(self, index: int) -> Process:
-        return self.__data[index]
+    def __getitem__(self, __i: int) -> Process:  # queue[2]
+        return self.__data[__i]
 
-    def __iter__(self) -> Generator:
-        for value in self.__data:
-            yield value
-
-    def __next__(self) -> Process:
-        self.__i += 1
-        try:
-            return self[self.__i]
-        except IndexError:
-            self.__i = 0
-            raise StopIteration
-
-    # def __str__(self) -> str:
-    #     pass
-
+    def __iter__(self):  # e.g.: print(*queue); for o in queue: print(o): ...
+        # for value in self.__data:
+        #     yield value
+        return iter(self.__data)
+    
     def pop(self) -> Process:
         return self.__data.pop()
 
     def add(self, process: Process) -> None:
         if isinstance(process, Process):
             self.__data.insert(0, process)
+        if isinstance(process, int):
+            self.__data.insert(0, Process(process))
         else:
             raise ValueError
-
-    # next = __next__  # python 2.x
-
-
-if __name__ == "__main__":
-    # tests
-
-    q = Queue()
-    del q
-
-    q = Queue([Process(i) for i in range(1, 5)])
-    print(*q)
-    q.add(Process(3))
-    print(*q)
-    print(q.pop())
-    print(*q)
